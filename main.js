2f9e6805735abfdd6c1f38ba9a8db598b8127941
sosuisen <hidekazu.kubota@gmail.com>
2025-03-28 10:26:12 +0900

    fix: v8

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ await app.init({ width: 768, height: 512 });
 // app.viewはcanvas要素
 document.body.appendChild(app.canvas);
 
-const txWidth = 256;
-const txHeight = 256;
+//const txWidth = 256;
+//const txHeight = 256;
 
 /**
  * 練習１
@@ -17,32 +17,38 @@ await PIXI.Assets.load('kyocotan256x256.png');
 const sprite = PIXI.Sprite.from('kyocotan256x256.png');
 app.stage.addChild(sprite);
 
-// ピクセルデータを取得
-// 得られるデータの型はUint8Array（8 ビット符号なし整数値の配列）
-const pixels = app.renderer.extract.pixels(sprite);
+// ピクセルデータのコピーを取得
+const pixelsOutput = app.renderer.extract.pixels(sprite);
+const { pixels, width, height } = pixelsOutput;
+// pixelsは8ビット符号なし整数値の配列
 console.log(pixels);
 
 // ピクセルデータを編集
-for (let x = 0; x < txWidth; x++) {
-    for (let y = 0; y < txHeight; y++) {
-        let r = pixels[y * txWidth * 4 + x * 4];
-        let g = pixels[y * txWidth * 4 + x * 4 + 1];
-        let b = pixels[y * txWidth * 4 + x * 4 + 2];
-        let a = pixels[y * txWidth * 4 + x * 4 + 3];
+for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+        let index = y * width + x;
+        let r = pixels[index * 4];
+        let g = pixels[index * 4 + 1];
+        let b = pixels[index * 4 + 2];
+        let a = pixels[index * 4 + 3];
         // r, g, b, aの値を変更(0-255)
         r = 255;
 
-        pixels[y * txWidth * 4 + x * 4] = r;
-        pixels[y * txWidth * 4 + x * 4 + 1] = g;
-        pixels[y * txWidth * 4 + x * 4 + 2] = b;
-        pixels[y * txWidth * 4 + x * 4 + 3] = a;
+        pixels[y * width * 4 + x * 4] = r;
+        pixels[y * width * 4 + x * 4 + 1] = g;
+        pixels[y * width * 4 + x * 4 + 2] = b;
+        pixels[y * width * 4 + x * 4 + 3] = a;
     }
 }
 
 // 編集後のピクセルデータからテクスチャ作成
-const newTexture = PIXI.Texture.fromBuffer(pixels, txWidth, txHeight);
+const newTexture = PIXI.Texture.from({
+    resource: pixels,
+    width,
+    height,
+});
 const newSprite = PIXI.Sprite.from(newTexture);
-newSprite.x = txWidth;
+newSprite.x = width;
 app.stage.addChild(newSprite);
 
 /**
@@ -50,7 +56,7 @@ app.stage.addChild(newSprite);
  * 白紙のビットマップから長方形の絵を描画
  */
 // 白紙のピクセルデータを作成
-const rectPixels = new Uint8Array(4 * txWidth * txHeight);
+const rectPixels = new Uint8Array(4 * width * height);
 // 描画する矩形
 const rectX = 64;
 const rectY = 96;
@@ -65,32 +71,59 @@ for (let x = rectX; x < rectX + rectWidth; x++) {
         let b = 0;
         let a = 255;
         // let a = 128;
-        rectPixels[y * txWidth * 4 + x * 4] = r;
-        rectPixels[y * txWidth * 4 + x * 4 + 1] = g;
-        rectPixels[y * txWidth * 4 + x * 4 + 2] = b;
-        rectPixels[y * txWidth * 4 + x * 4 + 3] = a;
+        let index = y * width + x;
+        rectPixels[index * 4] = r;
+        rectPixels[index * 4 + 1] = g;
+        rectPixels[index * 4 + 2] = b;
+        rectPixels[index * 4 + 3] = a;
     }
 }
 // 編集後のピクセルデータからテクスチャ作成
-const rectTexture = PIXI.Texture.fromBuffer(rectPixels, txWidth, txHeight);
+const rectTexture = PIXI.Texture.from({
+    resource: rectPixels,
+    width,
+    height
+});
 const rectSprite = PIXI.Sprite.from(rectTexture);
-rectSprite.y = txHeight;
+rectSprite.y = height;
 app.stage.addChild(rectSprite);
 
 /**
- * 発展課題
+ * 基本課題
+ * 既存のビットマップの色をグレースケールへ修正
+ */
+// 元絵からピクセルデータをコピー
+const pixelsOutput2 = app.renderer.extract.pixels(sprite);
+const { pixels: pixels2 } = pixelsOutput2;
+// ピクセルデータを編集
+
+
+
+
+// 編集後のピクセルデータからテクスチャ作成
+const grayTexture = PIXI.Texture.from({
+    resource: pixels2,
+    width,
+    height
+});
+const graySprite = PIXI.Sprite.from(grayTexture);
+graySprite.x = width * 2;
+app.stage.addChild(graySprite);
+
+
+/**
+ * 発展課題8a
  * 白紙のビットマップから市松模様を作成
  */
 // 白紙のピクセルデータを作成
-const ichimaPixels = new Uint8Array(4 * txWidth * txHeight);
-
+const ichimaPixels = new Uint8ClampedArray(4 * width * height);
 // 描画する矩形の範囲は練習２と同じ
 
 // ピクセルデータを編集
 for (let x = rectX; x < rectX + rectWidth; x++) {
     for (let y = rectY; y < rectY + rectHeight; y++) {
         let r, g, b, a;
-        if ((x % 10 < 5  && y % 10 < 5)
+        if ((x % 10 < 5 && y % 10 < 5)
             || (x % 10 >= 5 && y % 10 >= 5)
         ) {
             r = 255;
@@ -102,16 +135,20 @@ for (let x = rectX; x < rectX + rectWidth; x++) {
             g = 0;
             b = 0;
         }
-        ichimaPixels[y * txWidth * 4 + x * 4] = r;
-        ichimaPixels[y * txWidth * 4 + x * 4 + 1] = g;
-        ichimaPixels[y * txWidth * 4 + x * 4 + 2] = b;
-        ichimaPixels[y * txWidth * 4 + x * 4 + 3] = 255;
+        ichimaPixels[y * width * 4 + x * 4] = r;
+        ichimaPixels[y * width * 4 + x * 4 + 1] = g;
+        ichimaPixels[y * width * 4 + x * 4 + 2] = b;
+        ichimaPixels[y * width * 4 + x * 4 + 3] = 255;
     }
 }
 // 編集後のピクセルデータからテクスチャ作成
-const ichimaTexture = PIXI.Texture.fromBuffer(ichimaPixels, txWidth, txHeight);
+const ichimaTexture = PIXI.Texture.from({
+    resource: ichimaPixels,
+    width,
+    height
+});
 const ichimaSprite = PIXI.Sprite.from(ichimaTexture);
-ichimaSprite.x = txWidth;
-ichimaSprite.y = txHeight;
+ichimaSprite.x = width;
+ichimaSprite.y = height;
 app.stage.addChild(ichimaSprite);
 
